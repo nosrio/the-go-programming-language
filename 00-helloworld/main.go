d692a81e550ecfc862ecfbb89f0c879189c2f7fd
Nicolás Osorio <rnicolas.yo@gmail.com>
2023-09-19 09:39:56 -0300

    Change structure

--- 00-helloworld/main.go
+++ 00-helloworld/main.go
@@ -1,6 +1,9 @@
 // We’ll start wit h the now-t radition al ‘‘hel lo, wor ld’’ example, which app ears at the beg inning of
 // The C Programmi n g Lang uage, publishe d in 1978. C is one of the most direc t influences on
 // Go, and ‘‘hel lo, wor ld’’ illustrates a number of central ideas.
+
+// go run main.go
+// go build main.go
 package main
 
 import "fmt"
